07933e9e3547bcbbc343dc6b80d771aaaf1aafab
Rianne S <0896535@hr.nl>
2017-09-06 03:19:16 +0200

    Added sounds and pickups
    Added sound and pickups to the game.
    The sounds added were the sounds for jumping, picking up different
    pickups and dying.
    Pickups and pickup prefabs were added, but give no points yet.

---
+++ Assets/_Project/Scripts/Pickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Pickup : MonoBehaviour {
+    private AudioSource audio;
+    private Renderer render;
+
+    private bool triggered = false;
+
+    // Use this for initialization
+    void Start () {
+        audio = gameObject.GetComponent<AudioSource>();
+        render = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            render.enabled = false;
+            audio.Play();
+            Destroy(gameObject, 5);     //Delay object destruction, ohterwise sound will not be played
+            //Todo: add one to score
+        }
+    }
+}
