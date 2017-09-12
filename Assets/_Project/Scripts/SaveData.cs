b2068e9c69cb375d136cfc5b7bbd31c358f6973a
Martijn Rietveld <martijnrietveld@Martijns-MacBook-Pro.local>
2017-09-12 14:57:46 +0200

    Maps added, layout changed
    Added: Maps
    Added: Highscore & last run
    Changed: Portrait mode hard fix

---
+++ Assets/_Project/Scripts/SaveData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerPrefs.SetInt ("Highscore", 0);
+	}
+}
