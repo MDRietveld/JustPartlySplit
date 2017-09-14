1d9f188c3e76ad8adba4975f3911a9d64590fae6
Martijn Rietveld <martijnrietveld@msn.com>
2017-09-14 16:01:37 +0200

    Background change in game
    Background now changes after a while

--- Assets/_Project/Scripts/RepeatingBackground.cs
+++ Assets/_Project/Scripts/RepeatingBackground.cs
@@ -34,39 +34,66 @@ public class RepeatingBackground : MonoBehaviour
 	//Moves the object this script is attached to right in order to create our looping background effect.
 	private void RepositionBackground()
 	{
-		// Count for back background
-//		if (gameObject.tag == "BBGUp") {
-//			GameControl.instance.backUpMap++;
-//			repeatedBackMap = GameControl.instance.backUpMap;
-//		}
-//		// Count for front background
-//		if (gameObject.tag == "FBGUp") {
-//			GameControl.instance.frontUpMap++;
-//			repeatedFrontMap = GameControl.instance.frontUpMap;
-//		}
-//		if (gameObject.tag == "BBGDown") {
-//			GameControl.instance.backDownMap++;
-//			repeatedBackMap = GameControl.instance.backDownMap;
-//		}
-//		// Count for front background
-//		if (gameObject.tag == "FBGDown") {
-//			GameControl.instance.frontDownMap++;
-//			repeatedFrontMap = GameControl.instance.frontDownMap;
-//		}
-//
-//		if (gameObject.tag == "BBGDown" && (repeatedBackMap == 1 || repeatedBackMap == 2)) {
-//			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load ("Textures/Background/BackDown2",typeof(Sprite)) as Sprite;
-//		} else if(gameObject.tag == "BBGUp" && (repeatedBackMap == 1 || repeatedBackMap == 2)) {
-//			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load ("Textures/Background/BackUp2",typeof(Sprite)) as Sprite;
-//		}
-//
-//		if (gameObject.tag == "FBGDown" && (repeatedFrontMap == 2 || repeatedFrontMap == 3)) {
-//			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load ("Textures/Background/FrontDown2",typeof(Sprite)) as Sprite;
-//		} else if(gameObject.tag == "FBGUp" && (repeatedFrontMap == 2 || repeatedFrontMap == 3)) {
-//			gameObject.GetComponent<SpriteRenderer> ().sprite = Resources.Load ("Textures/Background/FrontUp2",typeof(Sprite)) as Sprite;
-//		}
-		//This is how far to the right we will move our background object, in this case, twice its length. This will position it directly to the right of the currently visible background object.
-		Vector3 groundOffSet = new Vector3(18f * 2f, 0, 0);
+        // Count for back background
+        if (gameObject.tag == "BBGUp")
+        {
+            GameControl.instance.backUpMap++;
+            repeatedBackMap = GameControl.instance.backUpMap;
+        }
+        // Count for front background
+        if (gameObject.tag == "FBGUp")
+        {
+            GameControl.instance.frontUpMap++;
+            repeatedFrontMap = GameControl.instance.frontUpMap;
+        }
+        if (gameObject.tag == "BBGDown")
+        {
+            GameControl.instance.backDownMap++;
+            repeatedBackMap = GameControl.instance.backDownMap;
+        }
+        // Count for front background
+        if (gameObject.tag == "FBGDown")
+        {
+            GameControl.instance.frontDownMap++;
+            repeatedFrontMap = GameControl.instance.frontDownMap;
+        }
+
+        if (gameObject.tag == "BBGDown" && repeatedBackMap == 1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/BackDown1.5", typeof(Sprite)) as Sprite;
+        }
+        else if (gameObject.tag == "BBGUp" && repeatedBackMap == 1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/BackUp1.5", typeof(Sprite)) as Sprite;
+        }
+        else if (gameObject.tag == "BBGDown" && (repeatedBackMap == 2 || repeatedBackMap == 3))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/BackDown2", typeof(Sprite)) as Sprite;
+        }
+        else if (gameObject.tag == "BBGUp" && (repeatedBackMap == 2 || repeatedBackMap == 3))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/BackUp2", typeof(Sprite)) as Sprite;
+        }
+
+
+        if (gameObject.tag == "FBGDown" && repeatedFrontMap == 3)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/FrontDown1.5", typeof(Sprite)) as Sprite;
+        }
+        else if (gameObject.tag == "FBGUp" && repeatedFrontMap == 3)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/FrontUp1.5", typeof(Sprite)) as Sprite;
+        }
+        else if (gameObject.tag == "FBGDown" && (repeatedFrontMap == 4 || repeatedFrontMap == 5))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/FrontDown2", typeof(Sprite)) as Sprite;
+        }
+        else if (gameObject.tag == "FBGUp" && (repeatedFrontMap == 4 || repeatedFrontMap == 5))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load("Textures/Background/FrontUp2", typeof(Sprite)) as Sprite;
+        }
+        //This is how far to the right we will move our background object, in this case, twice its length. This will position it directly to the right of the currently visible background object.
+        Vector3 groundOffSet = new Vector3(18f * 2f, 0, 0);
 
 		//Move this object from it's position offscreen, behind the player, to the new position off-camera in front of the player.
 		transform.position = (Vector3) transform.position + groundOffSet;
