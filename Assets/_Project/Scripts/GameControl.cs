80c3efec40415cadcd93f5f12d7e06ce7229618f
Martijn Rietveld <martijnrietveld@Martijns-MacBook-Pro.local>
2017-09-15 10:00:42 +0200

    Size change of text
    It is now resized over different phone sizes

--- Assets/_Project/Scripts/GameControl.cs
+++ Assets/_Project/Scripts/GameControl.cs
@@ -77,13 +77,13 @@ public class GameControl : MonoBehaviour
 
 
 			if (PlayerPrefs.HasKey ("Highscore") && PlayerPrefs.HasKey("PreviousRun")) {
-                highscoreText.GetComponentInChildren<Text>().text = "Highscore: " + PlayerPrefs.GetInt("Highscore");
-                previousRunText.GetComponentInChildren<Text>().text = "Last Run: " + PlayerPrefs.GetInt("PreviousRun");
+                highscoreText.GetComponentInChildren<Text>().text = "Highscore \n" + PlayerPrefs.GetInt("Highscore");
+				previousRunText.GetComponentInChildren<Text>().text = "Last Run \n" + PlayerPrefs.GetInt("PreviousRun");
             } else {
 				PlayerPrefs.SetInt ("Highscore", 0);
 				PlayerPrefs.SetInt ("PreviousRun", 0);
-                highscoreText.GetComponentInChildren<Text>().text = "Highscore: 0";
-                previousRunText.GetComponentInChildren<Text>().text = "Highscore: 0";
+				highscoreText.GetComponentInChildren<Text>().text = "Highscore \n0";
+				previousRunText.GetComponentInChildren<Text>().text = "Last Run \n0";
 			}
 
             instance = this;
