06d989358d20651399488431371348e8fe5fb70d
Martijn Rietveld <martijnrietveld@Martijns-MacBook-Pro.local>
2017-10-03 23:39:08 +0200

    Maps, prefabs, textures changed
    Many more maps, new textures, new prefabs

--- Assets/_Project/Scripts/CoursesLoader.cs
+++ Assets/_Project/Scripts/CoursesLoader.cs
@@ -9,7 +9,7 @@ public class CoursesLoader : MonoBehaviour {
     private GameObject[] mediumMaps;
     private GameObject[] hardMaps;
     private GameObject[] maps;
-	private GameObject[] currentMaps;
+	private List<GameObject> currentMaps = new List<GameObject>();
     private GameObject firstObject;
     private Vector2 objectPoolPosition = new Vector2(4f, 0);
     private int superEasyI;
@@ -19,10 +19,11 @@ public class CoursesLoader : MonoBehaviour {
     private int rand;
     private int currentMap = 0;
     private int loadedMaps = 0;
-	private int testing = 0;
+	private int difficulty = 1;
 
 	private int mapCourse = 1;
 	private int bgToCourse = 1;
+	private string mapTag = "Grass";
 
 //    private bool startLoading = true;
     // Use this for initialization
@@ -39,6 +40,11 @@ public class CoursesLoader : MonoBehaviour {
             hardI = hardMaps.GetLength(0);
             rand = Random.Range(0, superEasyI);
             instance = this;
+			foreach(GameObject map in easyMaps){
+				if (map.tag.Contains("Grass")) {
+					currentMaps.Add(map);
+				}
+			}
         }  else if (instance != this)
             Destroy(gameObject);
     }
@@ -48,24 +54,52 @@ public class CoursesLoader : MonoBehaviour {
 	}
 
 	public void MapsToUse(){
-		// use bgToCourse to load in the current maps
-
-		// Foreach the current difficulty map array
-		// Filter on TAG
-
-		// Fill the filtered tag in a new GameObject array (currentMaps)
+		currentMaps.Clear ();
+		if (bgToCourse == 1) {
+			mapTag = "Grass";
+		} else if (bgToCourse == 2) {
+			mapTag = "City";
+		} else if (bgToCourse == 3) {
+			mapTag = "Lava";
+		}
+		if (difficulty == 1) {
+			foreach (GameObject map in easyMaps) {
+				if (map.tag.Contains (mapTag)) {
+					currentMaps.Add (map);
+				}
+			}
+		} else if (difficulty == 2) {
+			foreach (GameObject map in mediumMaps) {
+				if (map.tag.Contains (mapTag)) {
+					currentMaps.Add (map);
+				}
+			}
+		} else {
+			foreach (GameObject map in hardMaps) {
+				if (map.tag.Contains (mapTag)) {
+					currentMaps.Add (map);
+				}
+			}
+		}
 	}
 
     public void LoadMap()
     {
 		mapCourse++;
-		testing += 9;
-		Debug.Log ("Course:" + testing);
+//		testing += 9;
+//		Debug.Log ("Course:" + testing);
 
 		if (mapCourse == 4) {
 			mapCourse = 0;
 			bgToCourse = GameControl.instance.goingToBg;
 			// Execute MapsToUse()
+			MapsToUse ();
+		}
+
+		if (loadedMaps % 5 == 0 && loadedMaps != 0) {
+			Debug.Log (loadedMaps);
+			difficulty++;
+			MapsToUse ();
 		}
 
         if (currentMap == 0) {
@@ -73,19 +107,23 @@ public class CoursesLoader : MonoBehaviour {
         } else {
             currentMap = 0;
         }
-        if (loadedMaps < 5) {
-			// Execute MapsToUse()
-            rand = Random.Range(0, easyI);
-            maps[currentMap] = (GameObject)Instantiate(easyMaps[rand], objectPoolPosition, Quaternion.identity);
-        } else if (loadedMaps < 10) {
-			// Execute MapsToUse()
-            rand = Random.Range(0, mediumI);
-            maps[currentMap] = (GameObject)Instantiate(mediumMaps[rand], objectPoolPosition, Quaternion.identity);
-        } else {
-			// Execute MapsToUse()
-            rand = Random.Range(0, hardI);
-            maps[currentMap] = (GameObject)Instantiate(hardMaps[rand], objectPoolPosition, Quaternion.identity);
-        }
+
+		rand = Random.Range(0, currentMaps.Count);
+		maps[currentMap] = (GameObject)Instantiate(currentMaps[rand], objectPoolPosition, Quaternion.identity);
+
+//        if (loadedMaps < 5) {
+//			// Execute MapsToUse()
+//            rand = Random.Range(0, easyI);
+//            maps[currentMap] = (GameObject)Instantiate(easyMaps[rand], objectPoolPosition, Quaternion.identity);
+//        } else if (loadedMaps < 10) {
+//			// Execute MapsToUse()
+//            rand = Random.Range(0, mediumI);
+//            maps[currentMap] = (GameObject)Instantiate(mediumMaps[rand], objectPoolPosition, Quaternion.identity);
+//        } else {
+////			 Execute MapsToUse()
+//            rand = Random.Range(0, hardI);
+//            maps[currentMap] = (GameObject)Instantiate(hardMaps[rand], objectPoolPosition, Quaternion.identity);
+//        }
         loadedMaps++;
     }
 }
